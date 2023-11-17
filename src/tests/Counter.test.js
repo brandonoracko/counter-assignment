eed3d38e089c098ab446357b656c3a0cd4b8f869
brandonoracko <orackob@hotmail.com>
2023-11-16 20:34:34 -0500

    Tests Added

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,41 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMsg = screen.getByText(/Counter/i);
+  expect(counterMsg).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const initialCount = screen.getByText(/0/i);
+  expect(initialCount).toBeInTheDocument();
+
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByRole('button', { name: /\+/i });
+  const countDisplay = screen.getByTestId('count');
+
+  userEvent.click(incrementButton);
+  expect(countDisplay).toHaveTextContent('1');
+
+  userEvent.click(incrementButton);
+  expect(countDisplay).toHaveTextContent('2');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByRole('button', { name: /\-/i });
+  const countDisplay = screen.getByTestId('count');
+
+  userEvent.click(incrementButton);
+  expect(countDisplay).toHaveTextContent('-1');
+
+  userEvent.click(incrementButton);
+  expect(countDisplay).toHaveTextContent('-2');
 });
